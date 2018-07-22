2373d59bef4515c10e51fa0d9f444dbfe605480a
Rick Voermans <ricky54321@live.nl>
2018-07-22 02:53:19 +0200

    heroku fix

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req, res) {
-  res.sendfile('src/index.html')
-})
+  res.sendfile('src/index.html');
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
