d20c42cc120b3790ac4ef97d0d0d3bb1c0463132
Rick Voermans <ricky54321@live.nl>
2018-07-22 07:25:58 +0200

    online font

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class ToolbarComponent implements OnInit {
   public val = 0;
+  public needed_xp = 10;
 
   constructor() {
     setInterval(() => {
