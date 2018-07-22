0ff1d74c0df5eb068e9492b08fd33fcb36920cf2
Rick Voermans <ricky54321@live.nl>
2018-07-22 03:26:12 +0200

    heroku fix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 const path = require('path');
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname + 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/less')));
 
-app.get('*', function(req, res) {
-  res.sendfile(path.join(__dirname, 'dist/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/less/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
