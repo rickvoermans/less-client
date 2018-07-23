0af15e735c442e8c3ce21b52a3bf5fc4242b6ea7
ALTERNATE\Felix <felixboons@hotmail.com>
2018-07-23 17:21:50 +0200

    all kinds of stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { OnsenModule } from 'ngx-onsenui';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ToolbarComponent } from './menu/toolbar/toolbar.component';
+import { MainInfoComponent } from './menu/main-info/main-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    MainInfoComponent
   ],
   imports: [
-    BrowserModule,
-    OnsenModule
+    BrowserModule
   ],
   providers: [],
-  schemas: [
-      CUSTOM_ELEMENTS_SCHEMA
-  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
