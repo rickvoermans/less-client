468cf613b73bf32d40b1386e5118535506061bbd
ALTERNATE\Felix <felixboons@hotmail.com>
2018-07-23 18:23:45 +0200

    clean-up fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './menu/toolbar/toolbar.component';
 import { MainInfoComponent } from './menu/main-info/main-info.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { TabViewComponent } from './tab-view/tab-view.component';
-import { PageCurrentComponent } from './pages/page-current/page-current.component';
+import { PageCurrentComponent } from './pages/current/page-current.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    MainInfoComponent
+    MainInfoComponent,
     TabViewComponent,
     PageCurrentComponent
   ],
